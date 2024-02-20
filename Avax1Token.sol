1ada091f19d5d53610374b2044dc7a1a55150f23
Vikash2398 <160607273+Vikash2398@users.noreply.github.com>
2024-02-20 17:53:42 +0530

    Update Avax1Token.sol

--- Avax1Token.sol
+++ Avax1Token.sol
@@ -1,1 +1,35 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SpiderManGame {
+    address public spiderMan;
+    address public villain;
+    bool public gameInProgress;
+
+    constructor() {
+        spiderMan = msg.sender;
+        gameInProgress = false;
+    }
+
+    function startGame(address _villain) public  {
+        require(_villain != address(0), "Invalid villain address");
+        villain = _villain;
+        gameInProgress = true;
+    }
+
+    function defeatVillain() public  {
+        gameInProgress = false;
+        villain = address(1);
+    }
+
+    function gameOver() public {
+        assert(gameInProgress);
+        gameInProgress = false;
+        villain = address(0);
+    }
+
+    function callRevert() public pure {
+        revert("This function intentionally reverts");
+    }
+}
